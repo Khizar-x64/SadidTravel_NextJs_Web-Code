c54c2dc6af9c0996d6eea7afd140ead2db2aa72d
Khizar Iqbal <khizariqbal1101aa001@gmail.com>
2025-10-09 19:25:01 +0000

    what is the leght of our header. recommeend me height width  for logo sv

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -7,7 +7,7 @@ export const Icons = {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 200 50"
-      className={cn("h-8 w-auto", className)}
+      className={cn("h-10 w-auto", className)}
       {...props}
     >
       <text x="10" y="35" fontFamily="var(--font-playfair-display), serif" fontSize="30" fontWeight="bold" fill="currentColor">
