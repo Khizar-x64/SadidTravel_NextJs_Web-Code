117ba66fe8a64c927bb534e0eba442c978adb49b
Khizar Iqbal <khizariqbal1101aa001@gmail.com>
2025-10-14 20:28:05 +0000

    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1

--- src/components/phone-fab.tsx
+++ src/components/phone-fab.tsx
@@ -4,17 +4,11 @@
 import Link from "next/link";
 
 const PhoneIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg width="48" height="48" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" {...props}>
-        <defs>
-            <linearGradient id="blueGradient" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop offset="0%" stopColor="#4FC3F7"/>
-            <stop offset="100%" stopColor="#1976D2"/>
-            </linearGradient>
-        </defs>
-        <circle cx="24" cy="24" r="24" fill="url(#blueGradient)" />
-        <path fill="#FFFFFF" d="M31.6,30.2c-1.1,0-2.2-0.2-3.2-0.6c-0.5-0.2-1.1-0.1-1.5,0.3l-2.2,2.2c-4.1-2.2-7.4-5.5-9.6-9.6l2.2-2.2
-            c0.4-0.4,0.5-1,0.3-1.5c-0.4-1-0.6-2.1-0.6-3.2c0-0.8-0.7-1.5-1.5-1.5h-5C10.7,14.1,10,14.8,10,15.6c0,10.5,8.5,19,19,19
-            c0.8,0,1.5-0.7,1.5-1.5v-5C33.1,30.9,32.4,30.2,31.6,30.2z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" width="256" height="256" viewBox="0 0 256 256" xmlSpace="preserve" {...props}>
+        <g style={{stroke: 'none', strokeWidth: 0, strokeDasharray: 'none', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeMiterlimit: 10, fill: 'none', fillRule: 'nonzero', opacity: 1}} transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
+            <circle cx="45" cy="45" r="45" style={{stroke: 'none', strokeWidth: 1, strokeDasharray: 'none', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeMiterlimit: 10, fill: 'rgb(33,150,243)', fillRule: 'nonzero', opacity: 1}} transform="  matrix(1 0 0 1 0 0) "/>
+            <path d="M 57.332 70.088 c -1.955 0.001 -4.074 -0.313 -6.32 -0.946 c -6.303 -1.777 -12.904 -5.885 -18.586 -11.568 c -5.682 -5.682 -9.79 -12.283 -11.568 -18.586 c -1.84 -6.528 -0.978 -11.999 2.429 -15.406 c 0.124 -0.124 0.25 -0.244 0.38 -0.36 c 0.033 -0.046 0.07 -0.09 0.112 -0.131 l 1.819 -1.819 c 0.877 -0.877 2.042 -1.36 3.282 -1.36 c 1.24 0 2.405 0.483 3.282 1.36 l 6.976 6.977 c 1.809 1.809 1.809 4.754 0 6.563 L 36.149 37.8 c -0.753 0.753 -0.753 1.979 0 2.732 L 49.468 53.85 c 0.729 0.729 2.002 0.729 2.732 0 l 2.988 -2.988 c 1.809 -1.808 4.755 -1.81 6.564 -0.001 l 6.977 6.977 c 0.876 0.876 1.359 2.042 1.359 3.281 s -0.483 2.405 -1.359 3.281 l -1.819 1.819 c -0.042 0.042 -0.087 0.08 -0.133 0.114 c -0.117 0.13 -0.236 0.256 -0.36 0.379 C 64.182 68.948 61.058 70.088 57.332 70.088 z" style={{stroke: 'none', strokeWidth: 1, strokeDasharray: 'none', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeMiterlimit: 10, fill: 'rgb(255,255,255)', fillRule: 'nonzero', opacity: 1}} transform=" matrix(1 0 0 1 0 0) " strokeLinecap="round"/>
+        </g>
     </svg>
 );
 
